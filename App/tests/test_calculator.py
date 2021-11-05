2b22ffe8a62b3409d42307fdb9c232f5d503ee2b
Smeet Kathiria <smeetkathiria@gmail.com>
2021-11-05 18:50:12 -0400

    fixed doc string error

--- App/tests/test_calculator.py
+++ App/tests/test_calculator.py
@@ -6,34 +6,36 @@ import pytest
 from calc_mod.calculator import Calculator
 
 def test_zero_division():
+    """test_zero_division"""
+
     with pytest.raises(ZeroDivisionError):
         1 / 0
 
 def test_simple_instance():
-    """Test Two Numbers"""
+    """test_simple_instance"""
     calculator = Calculator()
     isinstance(calculator,Calculator)
 
 def test_calculator_add():
-    """Test Two Numbers"""
+    """Test Addition"""
     calculator = Calculator()
     answer = calculator.add(1,2)
     assert answer == 3
 
 def test_calculator_subtract():
-    """Test Two Numbers"""
+    """Test Subtraction"""
     calculator = Calculator()
     answer = calculator.subtract(1,2)
     assert answer == -1
 
 def test_calculator_multiply():
-    """Test Two Numbers"""
+    """Test Multiplication"""
     calculator = Calculator()
     answer = calculator.multiply(6,2)
     assert answer == 12
 
 def test_calculator_divide():
-    """Test Two Numbers"""
+    """Test Division"""
     calculator = Calculator()
     answer = calculator.divide(4,2)
     assert answer == 2
