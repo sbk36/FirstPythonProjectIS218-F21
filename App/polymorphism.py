8434f04be7350c55679edf05f251632bea7a5dee
Smeet Kathiria <smeetkathiria@gmail.com>
2021-11-25 18:48:39 -0500

    updated polymorphism

--- App/polymorphism.py
+++ App/polymorphism.py
@@ -7,5 +7,4 @@ obj_add = Addition(1.0,2.0)
 obj_sub = Subtraction(2.0,1.0)
 
 for obj_calc in (obj_add, obj_sub):
-    """This shows how we can use same obj_calc object to call two different methods"""
     print(obj_calc.getresult())
