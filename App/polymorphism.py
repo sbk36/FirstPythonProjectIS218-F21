1c899e5f94b4cc4e72246fe863b615bc81742f3f
Smeet Kathiria <smeetkathiria@gmail.com>
2021-11-25 18:44:42 -0500

    added new files

--- App/polymorphism.py
+++ App/polymorphism.py
@@ -1,3 +1,4 @@
+"""This shows example polymorphism"""
 
 from calc.addition import Addition
 from calc.subtraction import Subtraction
@@ -6,4 +7,5 @@ obj_add = Addition(1.0,2.0)
 obj_sub = Subtraction(2.0,1.0)
 
 for obj_calc in (obj_add, obj_sub):
+    """This shows how we can use same obj_calc object to call two different methods"""
     print(obj_calc.getresult())
