4dc8d9af08f6f75edcc7d89ed790a1e8776b6838
Smeet Kathiria <smeetkathiria@gmail.com>
2021-11-25 18:38:11 -0500

    added new files

---
+++ App/polymorphism.py
@@ -0,0 +1,9 @@
+
+from calc.addition import Addition
+from calc.subtraction import Subtraction
+
+obj_add = Addition(1.0,2.0)
+obj_sub = Subtraction(2.0,1.0)
+
+for obj_calc in (obj_add, obj_sub):
+    print(obj_calc.getresult())
