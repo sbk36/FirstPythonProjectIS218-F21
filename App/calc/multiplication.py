38187b3371cedacd733d0625ab7f1bd50de977fd
Smeet Kathiria <smeetkathiria@gmail.com>
2021-11-25 19:51:29 -0500

    updated files

--- App/calc/multiplication.py
+++ App/calc/multiplication.py
@@ -1,11 +1,11 @@
-"""This is the addition calculation that is being inherits
+"""This is the multiplication calculation that is being inherits
 the value A and value B from the calculation class"""
 
 from calc.calculation import Calculation
 
-#This is how you extend the Addition class with the Calculation
+#This is how you extend the multiplication class with the Calculation
 class Multiplication(Calculation):
-    """The addition class has one method to get the result of the the calculation A and B come from
+    """The multiplication class has one method to get the result of the the calculation A and B come from
     the calculation parent class"""
     def getresult(self):
         """get result"""
